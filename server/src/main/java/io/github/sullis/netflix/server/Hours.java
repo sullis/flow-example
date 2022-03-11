074f00f66d39a3c21a5ec444b624d0c872bc7c18
sullis <github@seansullivan.com>
2022-03-11 09:02:23 -0800

    add whitespace

--- server/src/main/java/io/github/sullis/netflix/server/Hours.java
+++ server/src/main/java/io/github/sullis/netflix/server/Hours.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 
 public class Hours {
     public static final Range<Integer> RANGE = Range.closed(0, 23);
+
     public static IntStream stream() {
         return IntStream.rangeClosed(RANGE.lowerEndpoint(), RANGE.upperEndpoint());
     }
