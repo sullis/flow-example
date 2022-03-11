9cd9d79d01c3fe0f02cd0b60cae673ff7bd30d0a
sullis <github@seansullivan.com>
2022-03-11 09:01:12 -0800

    add HoursTest

--- server/src/main/java/io/github/sullis/netflix/server/Hours.java
+++ server/src/main/java/io/github/sullis/netflix/server/Hours.java
@@ -1,12 +1,10 @@
 package io.github.sullis.netflix.server;
 
 import com.google.common.collect.Range;
-
 import java.util.stream.IntStream;
 
 public class Hours {
     public static final Range<Integer> RANGE = Range.closed(0, 23);
-
     public static IntStream stream() {
         return IntStream.rangeClosed(RANGE.lowerEndpoint(), RANGE.upperEndpoint());
     }
---
+++ server/src/test/java/io/github/sullis/netflix/server/HoursTest.java
@@ -0,0 +1,26 @@
+package io.github.sullis.netflix.server;
+
+import org.junit.jupiter.api.Test;
+
+import static io.github.sullis.netflix.server.Hours.RANGE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HoursTest {
+    @Test
+    public void rangeCheck() {
+        assertThat(RANGE.contains(0)).isTrue();
+        assertThat(RANGE.contains(23)).isTrue();
+
+        assertThat(RANGE.contains(-1)).isFalse();
+        assertThat(RANGE.contains(24)).isFalse();
+        assertThat(RANGE.contains(25)).isFalse();
+    }
+
+    @Test
+    public void allValuesArePresent() {
+        final var list = Hours.stream().boxed().toList();
+        assertThat(list).hasSize(24);
+        assertThat(list).containsExactly(
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23);
+    }
+}
