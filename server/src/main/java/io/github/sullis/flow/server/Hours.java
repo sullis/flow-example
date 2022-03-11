e32bbb1fede405484006310dad88c0d51faeece6
sullis <github@seansullivan.com>
2022-03-11 14:33:47 -0800

    HOURS_PER_DAY

--- server/src/main/java/io/github/sullis/flow/server/Hours.java
+++ server/src/main/java/io/github/sullis/flow/server/Hours.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Range;
 import java.util.stream.IntStream;
 
 public class Hours {
+    public static final int HOURS_PER_DAY = 24;
     public static final Range<Integer> RANGE = Range.closed(0, 23);
 
     public static IntStream stream() {
--- server/src/test/java/io/github/sullis/flow/server/HoursTest.java
+++ server/src/test/java/io/github/sullis/flow/server/HoursTest.java
@@ -19,7 +19,7 @@ public class HoursTest {
     @Test
     public void allValuesArePresent() {
         final var list = Hours.stream().boxed().toList();
-        assertThat(list).hasSize(24);
+        assertThat(list).hasSize(Hours.HOURS_PER_DAY);
         assertThat(list).containsExactly(
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23);
     }
