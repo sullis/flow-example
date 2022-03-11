90cc0ff59eac6b8eb58450bd3ef8eabc9f9810b3
sullis <github@seansullivan.com>
2022-03-11 10:08:15 -0800

    AppTest.java

--- server/src/test/java/io/github/sullis/netflix/server/AppTest.java
+++ server/src/test/java/io/github/sullis/netflix/server/AppTest.java
@@ -2,9 +2,37 @@ package io.github.sullis.netflix.server;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.containsString;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+// import static io.restassured.RestAssured.given;
+
+@ExtendWith(DropwizardExtensionsSupport.class)
 public class AppTest {
+    private static final DropwizardAppExtension<AppConfig> APP = new DropwizardAppExtension<>(
+            App.class,
+            ResourceHelpers.resourceFilePath("test_config.yml")
+    );
+
+    private static String flowsEndpointUrl() {
+        return "http://localhost:" + APP.getLocalPort() + "/flows";
+    }
+
     @Test
-    public void happyPath() {
-        
+    public void happyPath() throws Exception {
+        /*
+        given()
+                .request()
+                .get(flowsEndpointUrl())
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType("application/json")
+                .body(containsString("Hello Obama")); */
     }
 }
