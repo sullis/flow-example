2c5312e44a52fd3d9815639ea174909b963a9acd
sullis <github@seansullivan.com>
2022-03-11 14:44:49 -0800

    update HoursTest

--- server/src/test/java/io/github/sullis/flow/server/HoursTest.java
+++ server/src/test/java/io/github/sullis/flow/server/HoursTest.java
@@ -1,6 +1,11 @@
 package io.github.sullis.flow.server;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.IntStream;
 
 import static io.github.sullis.flow.server.Hours.RANGE;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,5 +27,21 @@ public class HoursTest {
         assertThat(list).hasSize(Hours.HOURS_PER_DAY);
         assertThat(list).containsExactly(
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { -1, 25, 100 })
+    public void isValidHourReturnsFalse(final int hour) {
+        assertThat(Hours.isValidHour(hour)).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void isValidHourReturnsTrue(final int hour) {
+        assertThat(Hours.isValidHour(hour)).isTrue();
+    }
+
+    static private IntStream isValidHourReturnsTrue() {
+        return Hours.stream();
     }
 }
