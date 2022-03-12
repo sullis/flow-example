d52e27b11cf840c8f1b755b9037cb8d7d529033e
sullis <github@seansullivan.com>
2022-03-11 20:06:45 -0800

    add AppConcurrencyTest.java

---
+++ server/src/test/java/io/github/sullis/flow/server/AppConcurrencyTest.java
@@ -0,0 +1,95 @@
+package io.github.sullis.flow.server;
+
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junitpioneer.jupiter.cartesian.CartesianTest;
+import org.openapitools.model.FlowLog;
+
+import java.security.SecureRandom;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+
+import static io.github.sullis.flow.server.TestUtils.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+
+ This class uses the [dropwizard-testing] library
+ https://www.dropwizard.io/en/latest/manual/testing.html
+
+ */
+@ExtendWith(DropwizardExtensionsSupport.class)
+public class AppConcurrencyTest extends AbstractDropwizardTest {
+    private static final Random RANDOM = new SecureRandom();
+
+    private static final DropwizardAppExtension<AppConfig> APP = setupAppExtension();
+
+    @CartesianTest
+    public void concurrency(
+            @CartesianTest.Values(ints = { 1, 2, 10 }) final int numReaders,
+            @CartesianTest.Values(ints = { 1, 2, 10 }) final int numWriters) throws Exception {
+
+        final int defaultHour = 3;
+        final int numThreads = numReaders + numWriters;
+        final var executor = Executors.newFixedThreadPool(numThreads);
+        final var readers = makeReaders(numReaders, defaultHour);
+        final var writers = makeWriters(numWriters, defaultHour);
+        final var callables = new LinkedList<Callable<Boolean>>();
+        callables.addAll(readers);
+        callables.addAll(writers);
+
+        Collections.shuffle(callables, RANDOM);
+        final var futures = executor.invokeAll(callables);
+        waitForSuccess(futures, Duration.ofSeconds(5));
+
+        final var flows = getFlows(defaultHour);
+        assertThat(flows).hasSizeGreaterThan(0);
+        for (FlowLog flow: flows) {
+            assertThat(flow.getHour()).isEqualTo(3);
+            assertThat(flow.getBytesRx()).isGreaterThan(0);
+            assertThat(flow.getBytesTx()).isGreaterThan(0);
+            assertThat(flow.getBytesRx()).isNotEqualTo(flow.getBytesTx());
+            assertThat(Utils.isValid(flow));
+        }
+        executor.shutdown();
+    }
+
+    @Override
+    protected DropwizardAppExtension getExtension() {
+        return APP;
+    }
+
+    private List<Callable<Boolean>> makeWriters(final int numWriters, final int hour) {
+        List<Callable<Boolean>> result = new LinkedList<>();
+        for (int i = 0; i < numWriters; i++) {
+            Callable<Boolean> writer = () -> {
+                final var response = postFlows(List.of(
+                        makeFlowLog(hour, "vpc-0"),
+                        makeFlowLog(hour + 1, "vpc-0"),
+                        makeFlowLog(hour, "vpc-0")));
+
+                return response.getStatus() == 204;
+            };
+            result.add(writer);
+        }
+        return result;
+    }
+
+    private List<Callable<Boolean>> makeReaders(final int numReaders, final int hour) {
+        List<Callable<Boolean>> result = new LinkedList<>();
+        for (int i = 0; i < numReaders; i++) {
+            Callable<Boolean> reader = () -> {
+                return (getFlows(hour) != null);
+            };
+            result.add(reader);
+        }
+        return result;
+    }
+
+}
--- server/src/test/java/io/github/sullis/flow/server/AppTest.java
+++ server/src/test/java/io/github/sullis/flow/server/AppTest.java
@@ -3,7 +3,6 @@ package io.github.sullis.flow.server;
 import org.junit.jupiter.api.Test;
 
 import static io.github.sullis.flow.server.TestUtils.makeFlowLog;
-import static io.github.sullis.flow.server.TestUtils.waitForSuccess;
 import static io.github.sullis.flow.server.TestUtils.GET_RESPONSE_TYPE;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,18 +10,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
-import org.junitpioneer.jupiter.cartesian.CartesianTest;
 import org.openapitools.model.FlowLog;
 
 import javax.ws.rs.client.Entity;
-import java.security.SecureRandom;
-import java.time.Duration;
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
 
 /**
 
@@ -33,7 +24,6 @@ import java.util.concurrent.Executors;
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class AppTest extends AbstractDropwizardTest {
     private static final String CONTENT_TYPE = "application/json";
-    private static final Random RANDOM = new SecureRandom();
 
     private static final DropwizardAppExtension<AppConfig> APP = setupAppExtension();
 
@@ -67,35 +57,6 @@ public class AppTest extends AbstractDropwizardTest {
 
         assertThat(response)
             .containsExactlyInAnyOrder(expected0, expected1);
-    }
-
-    @CartesianTest
-    public void concurrency(
-            @CartesianTest.Values(ints = { 1, 2, 10 }) final int numReaders,
-            @CartesianTest.Values(ints = { 1, 2, 10 }) final int numWriters) throws Exception {
-
-        final int defaultHour = 3;
-        final int numThreads = numReaders + numWriters;
-        final var executor = Executors.newFixedThreadPool(numThreads);
-        final var readers = makeReaders(numReaders, defaultHour);
-        final var writers = makeWriters(numWriters, defaultHour);
-        final var callables = new LinkedList<Callable<Boolean>>();
-        callables.addAll(readers);
-        callables.addAll(writers);
-
-        Collections.shuffle(callables, RANDOM);
-        final var futures = executor.invokeAll(callables);
-        waitForSuccess(futures, Duration.ofSeconds(5));
-
-        final var flows = getFlows(defaultHour);
-        assertThat(flows).hasSizeGreaterThan(0);
-        for (FlowLog flow: flows) {
-            assertThat(flow.getHour()).isEqualTo(3);
-            assertThat(flow.getBytesRx()).isGreaterThan(0);
-            assertThat(flow.getBytesTx()).isGreaterThan(0);
-            assertThat(flow.getBytesRx()).isNotEqualTo(flow.getBytesTx());
-        }
-        executor.shutdown();
     }
 
     @Test
@@ -142,33 +103,6 @@ public class AppTest extends AbstractDropwizardTest {
     @Override
     protected DropwizardAppExtension getExtension() {
         return APP;
-    }
-
-    private List<Callable<Boolean>> makeWriters(final int numWriters, final int hour) {
-        List<Callable<Boolean>> result = new LinkedList<>();
-        for (int i = 0; i < numWriters; i++) {
-            Callable<Boolean> writer = () -> {
-                final var response = postFlows(List.of(
-                        makeFlowLog(hour, "vpc-0"),
-                        makeFlowLog(hour + 1, "vpc-0"),
-                        makeFlowLog(hour, "vpc-0")));
-
-                return response.getStatus() == 204;
-            };
-            result.add(writer);
-        }
-        return result;
-    }
-
-    private List<Callable<Boolean>> makeReaders(final int numReaders, final int hour) {
-        List<Callable<Boolean>> result = new LinkedList<>();
-        for (int i = 0; i < numReaders; i++) {
-            Callable<Boolean> reader = () -> {
-                return (getFlows(hour) != null);
-            };
-            result.add(reader);
-        }
-        return result;
     }
 
 }
